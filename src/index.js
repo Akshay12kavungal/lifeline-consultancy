5326c7bd4af4d1039ad446600367c5c1babe4ca5
Akshay.k <akshaykvl90@gmail.com>
2026-05-30 13:08:05 +0530

    fix: update Google Analytics tracking ID

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import ReactGA from 'react-ga4';
 
-ReactGA.initialize('G-5C31D1LBPR'); // paste your ID here
+ReactGA.initialize('G-6VLQW9RN2C'); // paste your ID here
 ReactGA.send('pageview');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
