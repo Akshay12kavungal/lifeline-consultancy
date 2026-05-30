1b267b8facb338e571a58d9591e7e2767a628efa
Akshay.k <akshaykvl90@gmail.com>
2026-05-30 12:45:56 +0530

    feat: Add Google Analytics integration with react-ga4

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import ReactGA from 'react-ga4';
+
+ReactGA.initialize('G-5C31D1LBPR'); // paste your ID here
+ReactGA.send('pageview');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<React.StrictMode><App /></React.StrictMode>);
